7fcfbf2f30291d8604db2816ceebfda45317158a
Goldcoders <goldcoders@protonmail.com>
2021-07-27 22:42:20 +0800

    added get_storage during init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/modules/home/views/home_view.dart';
 
 Future<void> main() async {
-  runApp(GetMaterialApp(home: HomeView()));
+  await GetStorage.init();
   await dotenv.load(fileName: ".env");
+  runApp(GetMaterialApp(home: HomeView()));
 }
