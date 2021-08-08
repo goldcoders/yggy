8481a3aae9f29af1a9dc0af32c8a6decd5eaebdc
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:25:46 +0800

    rename to package controller

--- lib/app/modules/home/controllers/home_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class HomeController extends GetxController {
-  final RxBool _isLoading = false.obs;
-  bool get isLoading => _isLoading.value;
-  void toggleLoading() => _isLoading.value = !_isLoading.value;
-}
