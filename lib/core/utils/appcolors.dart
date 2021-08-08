078f08f8e524b092666334df6a9de8c88d26f635
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:29:16 +0800

    add appcolors object

---
+++ lib/core/utils/appcolors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'constants.dart';
+
+const Map<String, Color> appColors = {
+  PRIMARY: Color(0xffc541da),
+  SECONDARY: Color(0xffb69859),
+  ACCENT: Color(0xFFFF4081),
+  BG_COLOR: Color(0xFFFFFFFF),
+  PRIMARY_DARK: Color(0xffc541da),
+  SECONDARY_DARK: Color(0xFFFF4081),
+  ACCENT_DARK: Color(0xffb69859),
+  BG_COLOR_DARK: Color(0x52351666),
+};
