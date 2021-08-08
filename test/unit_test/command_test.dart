8df564b627ab4df40895d891086c4fb553c25d8a
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:26:52 +0800

    add unit test for command

---
+++ test/unit_test/command_test.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:yggy/core/domain/repositories/command.dart';
+import 'package:yggy/core/utils/constants.dart';
+
+class FakeCommand extends Fake implements Command {
+  @override
+  Future<bool> locate(String command) async {
+    if (command == 'node' || command == 'webi') return Future.value(true);
+    return false;
+  }
+
+  @override
+  Future<String?> getVersion(String command, List<String> args) async {
+    if (command == 'node') {
+      final String version = dotenv.env[NODE_VERSION]!;
+      return Future.value(version);
+    } else {
+      return Future.value(null);
+    }
+  }
+}
+
+void main() {
+  final command = FakeCommand();
+
+  setUp(() {
+    print(Directory.current.toString());
+    dotenv.testLoad(
+        fileInput: File('.env')
+            .readAsStringSync()); //, mergeWith: Platform.environment
+  });
+
+  group('Command Exists in PATH', () {
+    test('Node Should Be Installed', () async {
+      final bool installed = await command.locate('node');
+      expect(installed, true);
+    });
+
+    test('Webi Should Be Installed', () async {
+      final bool installed = await command.locate('webi');
+      expect(installed, true);
+    });
+  });
+
+  group('Version Check', () {
+    test('Node Version Installed Should be v16.5.0', () async {
+      final List<String> args = ['--version'];
+      final String? version = await command.getVersion('node', args);
+      expect(version, 'v16.5.0');
+    });
+  });
+}
