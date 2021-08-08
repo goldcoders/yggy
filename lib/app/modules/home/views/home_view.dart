cc33b79543bbee21fcd526de8609b293a067da22
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:30:40 +0800

    update home to use packagecontroller

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,36 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../../commands/utils.dart';
-import '../controllers/home_controller.dart';
+import '../controllers/package_controller.dart';
 
-class HomeView extends GetView<HomeController> {
-  final homectl = Get.put(HomeController());
+class HomeView extends GetView<PackageController> {
   // use get find here we will make a new controller that is globally available
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: const Text('HomeView'),
         centerTitle: true,
       ),
       body: Center(
         child: TextButton(
-            child: Text('Check Node'),
             onPressed: () async {
-              // This is How we Use Our Get Command and Check Version Command
-              if (await getCommand('node', homectl)) {
-                List<String> args = ['-v'];
-                String? version = await checkVersion('node', args, homectl);
-                if (version != null) {
-                  Get.snackbar("Node", "version: $version");
-                }
-              }
-              // TODO: Handle Download if there is no command
+              print(controller.nodeInstalled);
+              print(controller.nodeVersion);
+              print(controller.webiInstalled);
               // TODO: Handle Upgrade if the version is outdated
               // TODO: Uninstall that Specific Command
               // TODO: Uninstall Specific Version of CLI
-            }),
+            },
+            child: const Text('Check Node')),
       ),
     );
   }
