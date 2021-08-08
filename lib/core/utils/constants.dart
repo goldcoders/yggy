7b242b9cb9193f272145572bffa0bc27c23c85c9
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:28:46 +0800

    add our app constants

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -2,13 +2,33 @@ const SESSION_KEY = 'GO_TRUE_SESSION';
 const SESSION_EXPIRY = 'GO_TRUE_EXPIRY';
 const SESSION_REFRESH_TOKEN = 'SESSION_REFRESH_TOKEN';
 
-const String API_URL = 'SB_URL';
-const String API_DOMAIN = 'SB_DOMAIN';
-const String API_KEY = 'SB_KEY';
-const String API_SECRET = 'SB_SECRET';
-const String HOST = 'SB_HOST';
+const String API_URL = 'API_URL';
+const String PRODUCTION = 'PRODUCTION';
+const String API_DOMAIN = 'API_DOMAIN';
+const String API_KEY = 'API_KEY';
+const String API_SECRET = 'API_SECRET';
+const String API_HOST = 'API_HOST';
 
 const String APP_LOGO = 'APP_LOGO';
 const String APP_NAME = 'APP_NAME';
 const String APP_VERSION = 'APP_VERSION';
 const String APP_COMPANY = 'APP_COMPANY';
+
+const String PRIMARY = 'primary';
+const String SECONDARY = 'secondary';
+const String ACCENT = 'accent';
+const String BG_COLOR = 'background_color';
+
+const String PRIMARY_DARK = 'primary_dark';
+const String SECONDARY_DARK = 'secondary_dark';
+const String ACCENT_DARK = 'accent_dark';
+const String BG_COLOR_DARK = 'background_color_dark';
+
+const String NODE_INSTALLED = 'NODE_INSTALLED';
+const String NODE_VERSION = 'NODE_VERSION';
+const String NODE_PATH = 'NODE_PATH';
+const String WEBI_INSTALLED = 'WEBI_INSTALLED';
+const String WEBI_PATH = 'WEBI_PATH';
+const String HUGO_INSTALLED = 'HUGO_INSTALLED';
+const String HUGO_PATH = 'HUGO_PATH';
+const String HUGO_VERSION = 'HUGO_VERSION';
