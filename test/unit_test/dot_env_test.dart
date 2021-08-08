4bb8ba2bffbde07e2a2f24020c57db6a99bdbd03
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:53:37 +0800

    added node version

--- test/unit_test/dot_env_test.dart
+++ test/unit_test/dot_env_test.dart
@@ -23,6 +23,7 @@ void main() {
       expect(dotenv.env[APP_VERSION], 'V1.0.0');
       expect(dotenv.env[APP_COMPANY], 'GOLDCODERS CORP.');
       expect(dotenv.env[PRODUCTION], 'true');
+      expect(dotenv.env[NODE_VERSION], 'v14.17.1');
     });
   });
 }
