802601310d0590f9729250b4c52afaab9395b398
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:31:03 +0800

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
-import 'app/modules/home/views/home_view.dart';
+import 'app.dart';
 
 Future<void> main() async {
   await GetStorage.init();
-  await dotenv.load(fileName: ".env");
-  runApp(GetMaterialApp(home: HomeView()));
+  await dotenv.load();
+  runApp(App());
 }
