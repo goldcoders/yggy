7610362702b65ef891bd0ef77a136025d5244b0f
Goldcoders <goldcoders@protonmail.com>
2021-08-09 00:26:41 +0800

    add unit test for dotenv

---
+++ test/unit_test/dot_env_test.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:test/test.dart';
+import 'package:yggy/core/utils/constants.dart';
+
+void main() {
+  group('dotenv', () {
+    setUp(() {
+      print(Directory.current.toString());
+      dotenv.testLoad(
+          fileInput: File('.env.test')
+              .readAsStringSync()); //, mergeWith: Platform.environment
+    });
+    test('able to load .env', () {
+      expect(dotenv.env[API_URL], 'https://test.supabase.co');
+      expect(dotenv.env[API_DOMAIN], 'goldcoders.dev');
+      expect(dotenv.env[API_KEY], 'SomeRandomString');
+      expect(dotenv.env[API_SECRET], 'secret');
+      expect(dotenv.env[API_HOST], 'localhost');
+      expect(dotenv.env[APP_LOGO], 'assets/images/app.png');
+      expect(dotenv.env[APP_NAME], 'YGGY');
+      expect(dotenv.env[APP_VERSION], 'V1.0.0');
+      expect(dotenv.env[APP_COMPANY], 'GOLDCODERS CORP.');
+      expect(dotenv.env[PRODUCTION], 'true');
+    });
+  });
+}
